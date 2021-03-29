57bf9486d31a5c9a7eda32185c5b72170adf681f
hackamro <lounis.amar.unity@gmail.com>
2021-03-29 23:25:14 +0100

    Signed-off-by: hackamro <lounis.amar.unity@gmail.com>

--- test.py
+++ test.py
@@ -32,56 +32,56 @@ def main() -> None:
     imgArray1 = fn.ReadImage2d_Array(path1)
     imgArray2 = fn.ReadImage2d_Array(path2)
 
-    # print("----------------------------------------- Egalisation d’histogramme  : Histogram origine")
-    # h1 = fn.Histogram_Array(imgArray1)
-    # Show_images([imgArray1,h1],"Histogram original picture")
+    print("----------------------------------------- Egalisation d’histogramme  : Histogram origine")
+    h1 = fn.Histogram_Array(imgArray1)
+    Show_images([imgArray1,h1],"Histogram original picture")
 
-    # print("----------------------------------------- Lissage des images : Filter Show")
-    # filte_v = np.array([[-1, 0, 1],
-    #                   [-2, 0, 2],
-    #                   [-1, 0, 1 ]
-    #                   ])
+    print("----------------------------------------- Lissage des images : Filter Show")
+    filte_v = np.array([[-1, 0, 1],
+                      [-2, 0, 2],
+                      [-1, 0, 1 ]
+                      ])
     
-    # filte_h = np.array([[-1, -2, -1],
-    #                   [0, 0, 0],
-    #                   [1, 2, 1 ]
-    #                   ])
+    filte_h = np.array([[-1, -2, -1],
+                      [0, 0, 0],
+                      [1, 2, 1 ]
+                      ])
     
-    # filter0 = np.array([[1 / 9, 1 / 9, 1 / 9],
-    #               [1 / 9, 1 / 9, 1 / 9],
-    #               [1 / 9, 1 / 9, 1 / 9]])
+    filter0 = np.array([[1 / 9, 1 / 9, 1 / 9],
+                  [1 / 9, 1 / 9, 1 / 9],
+                  [1 / 9, 1 / 9, 1 / 9]])
 
-    # imgOut = fn.Filter_Array(imgArray1,filter0)
-    # Show_images([imgArray1,imgOut],"Filter Moyen")
+    imgOut = fn.Filter_Array(imgArray1,filter0)
+    Show_images([imgArray1,imgOut],"Filter Moyen")
         
-    # imgOut = fn.Filter_Array(imgArray1,filte_h)
-    # Show_images([imgArray1,imgOut],"Filter Horizontal")
+    imgOut = fn.Filter_Array(imgArray1,filte_h)
+    Show_images([imgArray1,imgOut],"Filter Horizontal")
     
-    # imgOut = fn.Filter_Array(imgArray1,filte_v)
-    # Show_images([imgArray1,imgOut],"Filter vertical")
+    imgOut = fn.Filter_Array(imgArray1,filte_v)
+    Show_images([imgArray1,imgOut],"Filter vertical")
     
-    # print("----------------------------------------- Amélioration du contraste : Contrast Range")
-    # _min,_max = 30,220
-    # h1 = fn.Histogram_Array(imgArray1)
-    # imgOut = fn.ContrastRange_Array(imgArray1,_min,_max)
-    # h2 = fn.Histogram_Array(imgOut)
-    # Show_images([imgArray1,h1,imgOut,h2],"Histogram Contrast Range min:{0} | max:{1}".format(_min,_max) )
+    print("----------------------------------------- Amélioration du contraste : Contrast Range")
+    _min,_max = 30,220
+    h1 = fn.Histogram_Array(imgArray1)
+    imgOut = fn.ContrastRange_Array(imgArray1,_min,_max)
+    h2 = fn.Histogram_Array(imgOut)
+    Show_images([imgArray1,h1,imgOut,h2],"Histogram Contrast Range min:{0} | max:{1}".format(_min,_max) )
     
     
-    # print("----------------------------------------- Amélioration du contraste")
-    # imgOut1 = fn.ContrastLog_Array(imgArray1)
-    # imgOut2 = fn.ContrastLinear_Array(imgArray1,2,0)
-    # imgOut3 = fn.ContrastInvers_255_Array(imgArray1)
-    # Show_images([imgArray1,imgOut1,imgOut2,imgOut3],"Contrast Log Linear Invers")
+    print("----------------------------------------- Amélioration du contraste")
+    imgOut1 = fn.ContrastLog_Array(imgArray1)
+    imgOut2 = fn.ContrastLinear_Array(imgArray1,2,0)
+    imgOut3 = fn.ContrastInvers_255_Array(imgArray1)
+    Show_images([imgArray1,imgOut1,imgOut2,imgOut3],"Contrast Log Linear Invers")
     
-    # print("----------------------------------------- Segmentation par clustering")
-    # iris_x,iris_y,iris_r = fn.iris(imgArray1)
-    # pupil_x,pupil_y,pupil_r = fn.pupil(imgArray1)
+    print("----------------------------------------- Segmentation par clustering")
+    iris_x,iris_y,iris_r = fn.iris(imgArray1)
+    pupil_x,pupil_y,pupil_r = fn.pupil(imgArray1)
     
-    # imgIris = fn.zeroExternalArray(imgArray1,iris_x,iris_y,iris_r)
-    # imgIrisPupil = fn.zeroInternalArray(imgIris,pupil_x, pupil_y, pupil_r)
+    imgIris = fn.zeroExternalArray(imgArray1,iris_x,iris_y,iris_r)
+    imgIrisPupil = fn.zeroInternalArray(imgIris,pupil_x, pupil_y, pupil_r)
     
-    # Show_images([imgArray1,imgIrisPupil] , "Segmentation iris pupil")
+    Show_images([imgArray1,imgIrisPupil] , "Segmentation iris pupil")
     
     print("----------------------------------------- Opérations morphologiques : BiColor")
     imgBiColor = fn.Convert_BiColor_Array(imgArray1,50) # select pupil with color black
